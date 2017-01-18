03287554ce32ae37c00748d0bbed81cd48a5df1b
vinhkq <kieuquangvinh92@gmail.com>
2017-01-18 23:09:41 +0700

    Retrieve index data.

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,19 +1,12 @@
 import axios from 'axios';
-import cheerio from 'cheerio';
 import { BASE_URL } from '../config/constants';
+import { indexDOMParser } from '../helpers/domUtils';
 
 export default function index (res) {
   axios.get(BASE_URL)
   .then((response) => {
 
-    let $ = cheerio.load(response.data);
-
-    let items = $('.row .book-top').map(() => {
-      const rating = $(this).find("meta[itemprop='ratingValue']").attr('content');
-      const href = $(this).find('.btn-info').attr('href');
-      return { rating, href };
-    }).get();
-
+    const items = indexDOMParser(response.data);
     res.render('index', { title: 'Home Page!!!', items });
 
   }).catch(function (error) {
