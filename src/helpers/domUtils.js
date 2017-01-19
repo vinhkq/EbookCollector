fa6239839d78443fda2dd4fb6f4c48706c722d9b
vinhkq <kieuquangvinh92@gmail.com>
2017-01-20 00:21:36 +0700

    Retrieve basic data for detail page.

--- src/helpers/domUtils.js
+++ src/helpers/domUtils.js
@@ -7,18 +7,10 @@ export function indexDOMParser (data) {
     const image = $(item).find('.img-responsive').attr('src');
     const rating = $(item).find("meta[itemprop='ratingValue']").attr('content');
     const href = $(item).find('.btn-info').attr('href');
-
-    const authorList = $(item).find('.info').eq(1).find("a[rel='tag']").map((index, author) => {
-      return $(author).text();
-    }).get();
-    
-    const authorLinks = $(item).find('.info').eq(1).find("a[rel='tag']").map((index, author) => {
-      return $(author).attr('href');
-    }).get();
-
+    const authorList = $(item).find('.info').eq(1).find("a[rel='tag']").map((index, author) => $(author).text()).get();
+    const authorLinks = $(item).find('.info').eq(1).find("a[rel='tag']").map((index, author) => $(author).attr('href')).get();
     const publisher = $(item).find('.info').eq(2).find("a[rel='tag']").text();
     const publisherLink = $(item).find('.info').eq(2).find("a[rel='tag']").attr('href');
-
     const information = $(item).find('.info').eq(2).text();
 
     return {
@@ -36,4 +28,31 @@ export function indexDOMParser (data) {
   }).get();
 
     return items;
+}
+
+export function detailDOMParser (data) {
+  const $ = cheerio.load(data);
+
+  const name = $("h1[itemprop='name']").text();
+  const rating = $("meta[itemprop='ratingValue']").attr('content');
+  const image = $('.img-responsive').attr('src');
+  const authorList = $('.info').eq(1).find("a[rel='tag']").map((index, author) => $(author).text()).get();
+  const authorLinks = $('.info').eq(1).find("a[rel='tag']").map((index, author) => $(author).attr('href')).get();
+  const publisher = $('.info').eq(2).find("a[rel='tag']").text();
+  const publisherLink = $('.info').eq(2).find("a[rel='tag']").attr('href');
+  const publishDate = $('.info').eq(3).find('i').text();
+  const pages = $('.info').eq(4).find('i').text();
+  let description = $('.panel-body').eq(1).toArray(); // => todo
+  return {
+    name,
+    image,
+    rating,
+    authorList,
+    authorLinks,
+    publisher, 
+    publisherLink,
+    publishDate,
+    pages,
+    description
+  };
 }
