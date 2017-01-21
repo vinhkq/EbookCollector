6404e1e68ac719bc7204a85e397b12c94247c6a4
vinhkq <kieuquangvinh92@gmail.com>
2017-01-21 23:10:47 +0700

    Resolved description problem.

--- src/helpers/domUtils.js
+++ src/helpers/domUtils.js
@@ -42,8 +42,7 @@ export function detailDOMParser (data) {
   const publisherLink = $('.info').eq(2).find("a[rel='tag']").attr('href');
   const publishDate = $('.info').eq(3).find('i').text();
   const pages = $('.info').eq(4).find('i').text();
-  let description = $('.panel-body').eq(1).html();
-  description = description.substring(4);
+  const description = $('.panel-body').eq(1).html();
   return {
     name,
     image,
